837ec04be36a7a3234750a8384d2e77bbab3f78c
sioiyan <sioiyan3456@gmail.com>
2019-04-04 22:28:02 +0900

    let

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 'use strict';
 
-// 定数
+// 定数 const
+// 変数 let
 
 
 // console.log(150 * 140);
 // console.log(150 * 160);
 
-const price = 150
+let price = 150
+
+console.log(price * 140);
+console.log(price * 160);
+
+price = 170
 
 console.log(price * 140);
 console.log(price * 160);

82453bd5eab965f2417da6c5e3d009b4df37bd08
sioiyan <sioiyan3456@gmail.com>
2019-04-04 23:41:32 +0900

    for-loop

--- js/main.js
+++ js/main.js
@@ -1,28 +1,8 @@
 'use strict';
 
-const signal = 'hoge';
+// for
 
-// if (signal === 'red') {
-//     console.log('stop!');
-// }
-// else if (signal === 'yellow') {
-//     console.log('caution!');
-// }
-// else if (signal === 'blue') {
-//     console.log('go!');
-// }
-
-switch (signal) {
-    case 'red':
-        console.log('stop!');
-        break;
-    case 'yellow':
-        console.log('caution!');
-        break;
-    case 'blue':
-    case 'green':
-        console.log('go!');
-        break;
-    default:
-        console.log('wrong signal')
+for (let i = 1; i <= 10; i++) {
+    // console.log(i + 'hello');
+    console.log(`${i} hello`);
 }

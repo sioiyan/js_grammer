7bd805a1067d40da9e76a5dcd607a0f4a7d3df2e
sioiyan <sioiyan3456@gmail.com>
2019-04-05 00:11:47 +0900

    function expression

--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@
 
 // 関数
 
-function sum(a, b, c) {
-    // console.log(a + b + c);
-    return a + b + c
-}
+// function sum(a, b, c) {
+//     return a + b + c;
+// }
+const sum = function(a, b, c) {
+    return a + b + c;
+};
 
 // sum(1, 2, 3);
 // sum(3, 4, 5);
 
-const total = sum(1, 2, 3) + sum(3, 4, 5)
+const total = sum(1, 2, 3) + sum(3, 4, 5);
 // const total = 6 + 12;
 
 console.log(total);

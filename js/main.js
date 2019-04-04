5eedc343bd03784d2e6c0edf32f26ff019895949
sioiyan <sioiyan3456@gmail.com>
2019-04-04 23:55:45 +0900

    continue,break

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 'use strict';
 
-// while
-
-// let hp = 200;
-
-// while (hp > 0) {
-//     console.log(`${hp} HP left!`);
-//     hp -= 15;
-// }
-
-for (let hp = 200; hp > 0; hp -= 15) {
-    console.log(`${hp} HP left!`);
+for (let i = 1; i <= 10; i++) {
+    // if (i % 3 === 0) {
+    //     continue;
+    // }
+    if (i === 4){
+        break;
+    }
+    console.log(i);
 }

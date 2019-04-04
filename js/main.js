fae561db482204028f6457ce4132209e1f56f671
sioiyan <sioiyan3456@gmail.com>
2019-04-04 23:27:34 +0900

    ternary operator

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@
 
 const score = 40;
 
-if (score >= 80) {
-    console.log('Great!')
-} else if (score >= 60) {
-    console.log("Good");
-} else {
-    console.log('OK');
-}
+// if (score >= 80) {
+//     console.log('Great!')
+// } else {
+//     console.log('OK...!');
+// }
+
+score >= 80 ? console.log('Great!') : console.log('OK...!');
